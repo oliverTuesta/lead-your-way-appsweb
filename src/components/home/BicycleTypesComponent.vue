73e297e790c6f441b645050756f396367396a5a0
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-05-03 15:46:52 -0500

    options
    Aggregation of the 'option' section and its components in i18n, translation and calling of the same in BicycleTypesComponent.vue

--- src/components/home/BicycleTypesComponent.vue
+++ src/components/home/BicycleTypesComponent.vue
@@ -2,41 +2,38 @@
   <div>
     <div class="surface-section px-4 py-8 md:px-6 lg:px-8 text-center">
       <div class="mb-3 font-bold text-3xl">
-        <span class="text-900">One Product, </span>
-        <span class="text-orange-600">Many Solutions</span>
+        <span class="text-900">Lead Your Way, </span>
+        <span class="text-orange-600">{{ $t('options-title') }}</span>
       </div>
-      <div class="text-700 mb-6">
+      <!--<div class="text-700 mb-6">
         Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna.
-      </div>
+      </div>-->
       <div class="grid">
         <div class="col-12 md:col-4 mb-4 px-5">
           <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
             <i class="pi pi-heart-fill text-4xl text-orange-500"></i>
           </span>
-          <div class="text-900 text-xl mb-3 font-medium">Easy to Use</div>
+          <div class="text-900 text-xl mb-3 font-medium">{{ $t('option1') }}</div>
           <span class="text-700 line-height-3"
-            >Ornare suspendisse sed nisi lacus sed viverra tellus. Neque volutpat ac tincidunt vitae
-            semper.</span
+            >{{ $t('option1-content') }}</span
           >
         </div>
         <div class="col-12 md:col-4 mb-4 px-5">
           <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
             <i class="pi pi-credit-card text-4xl text-orange-500"></i>
           </span>
-          <div class="text-900 text-xl mb-3 font-medium">Open Source</div>
+          <div class="text-900 text-xl mb-3 font-medium">{{ $t('option2') }}</div>
           <span class="text-700 line-height-3"
-            >Nec tincidunt praesent semper feugiat. Sed adipiscing diam donec adipiscing tristique
-            risus nec feugiat.
+            >{{ $t('option2-content') }}
           </span>
         </div>
         <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
           <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
             <i class="pi pi-shield text-4xl text-orange-500"></i>
           </span>
-          <div class="text-900 text-xl mb-3 font-medium">Trusted Security</div>
+          <div class="text-900 text-xl mb-3 font-medium">{{ $t('option3') }}</div>
           <span class="text-700 line-height-3"
-            >Mattis rhoncus urna neque viverra justo nec ultrices. Id cursus metus aliquam
-            eleifend.</span
+            >{{ $t('option3-content') }}</span
           >
         </div>
       </div>
--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -8,25 +8,57 @@ const i18n = createI18n({
       lyw: 'Lead Your Way',
       'about-content':
         'Somos una compañía que ofrece una plataforma digital donde podrás encontrar bicicletas para todo tipo de usuarios Por lo que, lo que necesitas es una bicicleta tanto para practicar deporte o necesitas otro tipo de movilidad, en esta página encontrarás todo lo necesario de forma rápida, sencilla y segura. Además, nunca tendrás que preocuparte por no encontrar la bicicleta adecuada, en este lugar podrás encontrar lo que necesites según tus necesidades',
-    },
+      'options-title': 'es la forma más fácil de buscar bicicletas para...',
+      //'call-to-action-sub': '¡Encuentra tu bicicleta!',
+      'option1':'Ruta',
+      'option1-content':'La bicicleta de ruta se utiliza para describir bicicletas construidas para viajar a gran velocidad en carreteras pavimentadas. Sus características están pensadas para llevarte tan lejos y tan rápido como tus piernas lo permitan. Tienen un diseño con ángulos de asiento y frontales muy verticales, un eje pedalier alto y muy poca curvatura de la horquilla.',
+      'option2':'Montaña',
+      'option2-content':'Son bicicletas que, o bien carecen de suspensión o tienen un sistema de amortiguación delantera en la horquilla. Normalmente el recorrido de esta suspensión es de menos de 120mm, ya que están pensadas para rodar a toda velocidad por pistas y tramos sin asfaltar sin grandes desniveles ni obstáculos pronunciados.',
+      'option3':'Ciudad',
+      'option3-content':'Una bicicleta urbana se caracteriza por tener componentes simples pero resistentes a cualquier picadura. Su mantenimiento es más sencillo y por lo tanto económico. En comparación con una bicicleta de montaña suelen ser más pesadas y no requieres suspensiones para los recorridos en la ciudad.',
+      },
 
     en: {
       lyw: 'Lead Your Way',
       'about-content':
         'We are a company that offers a digital platform where you can find bicycles for all types of users. So, what you need is a bicycle both to practice sports or you need another type of mobility, on this page you will find everything you need quickly, easily and safely. Also, you will never have to worry about not finding the right bike, in this place you can find what you need according to your needs',
-    },
+      'options-title': 'is the easiest way to find bicycles for...',
+      //'call-to-action-sub': 'Find your bike!',
+      'option1':'Road',
+      'option1-content':'The road bike is used to describe bicycles built to travel at high speed on paved roads. Its features are designed to take you as far and as fast as your legs will allow. They have a design with very vertical seat and front angles, a high bottom bracket and very little fork curvature.',
+      'option2':'Mountain',
+      'option2-content':'They are bicycles that either lack suspension or have a front suspension system in the fork. Normally the travel of this suspension is less than 120mm, since they are designed to roll at full speed on tracks and unpaved sections without large slopes or pronounced obstacles.',
+      'option3':'City',
+      'option3-content':'An urban bicycle is characterized by having simple but resistant components to any puncture. Its maintenance is simpler and therefore cheaper. Compared to a mountain bike they are usually heavier and do not require suspensions for city trips.',
+      },
 
     fr: {
       lyw: 'Lead Your Way',
       'about-content':
         'Nous sommes une entreprise qui propose une plateforme numérique où vous pouvez trouver des vélos pour tous les types d’utilisateurs. Donc, ce dont vous avez besoin est un vélo à la fois pour pratiquer des sports ou vous avez besoin d’un autre type de mobilité, sur cette page vous trouverez tout ce dont vous avez besoin rapidement, facilement et en toute sécurité. De plus, vous n’aurez jamais à vous soucier de ne pas trouver le bon vélo, dans cet endroit vous pouvez trouver ce dont vous avez besoin en fonction de vos besoins',
-    },
+      'options-title': 'est le moyen le plus simple de trouver des vélos pour...',
+      //'call-to-action-sub': 'Trouvez votre vélo!',
+      'option1':'Route',
+      'option1-content':'Le vélo de route est utilisé pour décrire les vélos construits pour voyager à grande vitesse sur des routes pavées. Ses caractéristiques sont conçues pour vous emmener aussi loin et aussi vite que vos jambes le permettront. Ils ont une conception avec des angles de siège et de face très verticaux, un boîtier de pédalier haut et très peu de courbure de fourche.',
+      'option2':'Montagne',
+      'option2-content':'Ce sont des vélos qui manquent de suspension ou qui ont un système de suspension avant dans la fourche. Normalement, le déplacement de cette suspension est inférieur à 120 mm, car ils sont conçus pour rouler à pleine vitesse sur des pistes et des sections non pavées sans grandes pentes ni obstacles prononcés.',
+      'option3':'Ville',
+      'option3-content':'Un vélo urbain se caractérise par des composants simples mais résistants à toute crevaison. Son entretien est plus simple et donc moins cher. Par rapport à un VTT, ils sont généralement plus lourds et ne nécessitent pas de suspensions pour les trajets en ville.',
+      },
 
     ja: {
       lyw: 'Lead Your Way',
       'about-content':
         '私たちは、すべてのタイプのユーザーのための自転車を見つけることができるデジタルプラットフォームを提供している会社です。したがって、あなたがスポーツを練習するために自転車を必要としているか、あなたは別のタイプのモビリティを必要としているかどうか、このページでは、あなたが必要とするすべてのものを迅速かつ簡単に安全に見つけることができます。また、あなたは決して適切な自転車を見つけることを心配する必要はありません、この場所では、あなたのニーズに応じて必要なものを見つけることができます',
-    },
+      'options-title': 'は...のための自転車を見つける最も簡単な方法です',
+      //'call-to-action-sub': 'あなたの自転車を見つける！',
+      'option1':'道路',
+      'option1-content':'ロードバイクは、舗装された道路で高速で移動するために作られた自転車を表すために使用されます。その特徴は、あなたの足が許す限り遠く、速くあなたを連れて行くように設計されています。彼らは非常に垂直なシートとフロントアングル、高いボトムブラケット、そして非常に少ないフォーク曲率を持つデザインを持っています。',
+      'option2':'山',
+      'option2-content':'それらはサスペンションを欠いているか、フォークに前部サスペンションシステムを備えています。通常、このサスペンションの移動は120mm未満であり、大きな傾斜や顕著な障害物のないトラックや未舗装の区間でフルスピードで走行するように設計されています。',
+      'option3':'市',
+      'option3-content':'都市の自転車は、どんなパンクにも耐えられるシンプルで丈夫なコンポーネントを持っていることを特徴としています。そのメンテナンスはよりシンプルで、したがってより安価です。マウンテンバイクと比較して、通常より重く、都市の旅行にはサスペンションは必要ありません。',
+      },
   },
 
   // something vue-i18n options here ...
