e195b25096df471f3718266d7db5c6258fc00d2b
leonel1301 <u202115733@upc.edu.pe>
2023-06-24 23:52:01 -0500

    resolve bugs

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -22,7 +22,7 @@ import { cardService } from '../services/card.lyw.service';
                 <div class="card">
                   <div class="card-body">
                     <div class="d-flex flex-column align-items-center text-center">
-                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                      <img class="rounded-circle" width="150" :src="user.image" alt="Admin">
                       <div class="mt-3">
                         <h4>{{ user.name }}</h4>
                         <p class="text-secondary mb-1">{{ user.email }}</p>
@@ -91,7 +91,7 @@ import { cardService } from '../services/card.lyw.service';
                     <hr>
                     <div class="row">
                       <div class="col-sm-12">
-                        <a class="btn btn-info " target="__blank">Edit Profile</a>
+                        <a href="/profile/update" class="btn btn-info " target="__blank">Edit Profile</a>
                       </div>
                     </div>
                   </div>
