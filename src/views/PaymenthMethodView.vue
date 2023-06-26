4c97be56dab1f7b80612d0bf791d0d0e8517b5e1
leonel1301 <u202115733@upc.edu.pe>
2023-06-26 10:54:16 -0500

    feature: add paymenth method

--- src/views/PaymenthMethodView.vue
+++ src/views/PaymenthMethodView.vue
@@ -1,28 +1,46 @@
 <template>
+  <div>
+    <app-user-header></app-user-header>
     <div class="container">
-      <form>
+      <form @submit="onSubmit" ref="PaymentForm">
         <div class="row">
           <h1 class="title">Métodos de pago</h1>
           <div class="col">
             <div class="inputBox">
               <label for="nombre">Nombre completo :</label>
               <input
-                v-model="PaymentData.nombre"
+                v-model="PaymentData.name"
                 type="text"
                 id="nombre"
                 placeholder="Example Name"
                 required
+                name="nombre"
               />
             </div>
-  
+
             <div class="inputBox">
               <label for="card">Tarjetas que aceptamos :</label>
-              <img src="../assets/img/card_img.png" alt="" />
+              <RadioButton
+                v-model="PaymentData.cardType"
+                name="cardType"
+                value="Visa"
+                inputId="visa"
+              >
+                Visa
+              </RadioButton>
+              <RadioButton
+                v-model="PaymentData.cardType"
+                name="cardType"
+                value="Mastercard"
+                inputId="mastercard"
+              >
+                Mastercard
+              </RadioButton>
             </div>
             <div class="inputBox">
               <label for="numero_tarjeta">Número de tarjeta :</label>
               <input
-                v-model="PaymentData.numero_tarjeta"
+                v-model="PaymentData.cardNumber"
                 type="text"
                 id="numero_tarjeta"
                 placeholder="1111-2222-3333-4444"
@@ -32,7 +50,7 @@
             <div class="inputBox">
               <label for="nombre_tarjeta">Nombre de su tarjeta :</label>
               <input
-                v-model="PaymentData.nombre_tarjeta"
+                v-model="PaymentData.cardHolder"
                 type="text"
                 id="nombre_tarjeta"
                 placeholder="Example card name"
@@ -40,12 +58,12 @@
               />
             </div>
           </div>
-  
+
           <div class="col">
             <div class="inputBox">
               <label for="email">Correo electrónico :</label>
               <input
-                v-model="PaymentData.correo"
+                v-model="PaymentData.email"
                 type="email"
                 id="correo"
                 placeholder="example@example.com"
@@ -66,7 +84,7 @@
               <div class="inputBox">
                 <label for="mes_exp">Mes de exp :</label>
                 <input
-                  v-model="PaymentData.mes_exp"
+                  v-model="PaymentData.exp_month"
                   type="text"
                   id="mes_exp"
                   placeholder="05"
@@ -86,120 +104,260 @@
             </div>
           </div>
         </div>
-        <button type="submit" class="submit-btn">Añadir método</button>
+        <button type="submit" class="submit-btn btn-primary">
+          Añadir método
+        </button>
       </form>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        PaymentData: {
-          nombre: '',
-          numero_tarjeta: '',
-          nombre_tarjeta: '',
-          correo: '',
-          exp_year: '',
-          mes_exp: '',
-          cvv: '',
-        },
-      };
+  </div>
+</template>
+
+<script>
+import { cardService } from '../services/card.lyw.service';
+
+export default {
+  name: 'PaymentMethodView',
+  async mounted() {
+    const id = localStorage.getItem('id');
+    await cardService.getByUserId(id).then((response) => {
+      this.userId = response;
+    });
+  },
+  data() {
+    return {
+      PaymentData: {
+        name: '',
+        cardNumber: '',
+        cardHolder: '',
+        email: '',
+        exp_year: '',
+        exp_month: '',
+        cvv: '',
+        cardType: '',
+      },
+      userId: null,
+    };
+  },
+  methods: {
+    async onSubmit() {
+      if (!(await this.validForm())) return;
+      if (!(await this.createCard())) {
+        this.errorToast('Hubo un error al crear la cuenta');
+        return;
+      }
+      try {
+        await this.create();
+        this.successToast();
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (error) {
+        console.error('Error creating payment method:', error);
+        this.errorToast('Hubo un error al crear el método de pago');
+      }
     },
-  };
-  </script>
-  
-  <style scoped>
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap');
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 25px;
-    background-color: #f2f2f2;
-    font-family: 'Montserrat', sans-serif;
+    async validForm() {
+      if (!this.PaymentData.name) {
+        this.errorToast('Por favor ingrese su nombre completo');
+        return false;
+      }
+
+      if (!isValidCardNumber(this.PaymentData.cardNumber)) {
+        this.errorToast('Número de tarjeta inválido');
+        return false;
+      }
+
+      if (!this.PaymentData.cardHolder) {
+        this.errorToast('Por favor ingrese el nombre de su tarjeta');
+        return false;
+      }
+
+      if (!isValidEmail(this.PaymentData.email)) {
+        this.errorToast('Correo electrónico inválido');
+        return false;
+      }
+
+      const expYear = parseInt(this.PaymentData.exp_year, 10);
+      const expMonth = parseInt(this.PaymentData.mes_exp, 10);
+      const currentYear = new Date().getFullYear();
+      const currentMonth = new Date().getMonth() + 1;
+
+      if (
+        expYear < currentYear ||
+        (expYear === currentYear && expMonth < currentMonth)
+      ) {
+        this.errorToast('La tarjeta ha expirado');
+        return false;
+      }
+
+      if (
+        !/^\d{3,4}$/.test(this.PaymentData.cvv)
+      ) {
+        this.errorToast('CVV inválido');
+        return false;
+      }
+
+      return true;
+    },
+    async createCard() {
+      try {
+        const cardData = {
+          name: this.PaymentData.name,
+          number: this.PaymentData.cardNumber,
+          expirationDate: `${this.PaymentData.exp_year}-${this.PaymentData.exp_month}-01T00:00:00.000Z`,
+          cvv: this.PaymentData.cvv,
+          type: this.PaymentData.cardType,
+          userId: this.userId,
+        };
+
+        const response = await cardService.create(cardData);
+        return response.data;
+      } catch (error) {
+        console.error('Error creating card:', error);
+        throw error;
+      }
+    },
+    async successToast() {
+        this.$toast.add({
+        severity: 'success',
+        summary: 'Éxito',
+        detail: 'Método de pago creado',
+        life: 3000,
+      });
+    },
+    async errorToast(message) {
+        this.$toast.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: message,
+        life: 3000,
+      });
+    },
+  },
+};
+
+function isValidCardNumber(cardNumber) {
+  const cleanedCardNumber = cardNumber.replace(/[\s-]/g, '');
+
+  if (!/^\d+$/.test(cleanedCardNumber)) {
+    return false;
   }
-  
-  .container form {
-    max-width: 500px;
-    background: #fff;
-    border: 2px solid #d78477;
-    border-radius: 10px;
-    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-    padding: 30px;
+
+  let sum = 0;
+  let shouldDouble = false;
+
+  for (let i = cleanedCardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(cleanedCardNumber.charAt(i), 10);
+
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+
+    sum += digit;
+    shouldDouble = !shouldDouble;
   }
-  
-  .container form .row {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 15px;
-  }
-  
-  .container form .row .col {
-    flex: 1 1 50%;
-  }
-  
-  .container form .row .col .inputBox {
-    margin-bottom: 15px;
-  }
-  
-  .container form .row .col .inputBox label {
-    display: block;
-    margin-bottom: 5px;
-    font-weight: 700;
-  }
-  
-  .container form .row .col .inputBox input {
-    width: 100%;
-    border: 1px solid #ccc;
-    padding: 10px 15px;
-    font-size: 15px;
-  }
-  
-  .container form .row .col .inputBox input:focus {
-    border: 1px solid #d78477;
-  }
-  
-  .container form .row .col .inputBox img {
-    height: 30px;
-    margin-top: 5px;
-    filter: drop-shadow(0 0 1px #000);
-    transition: transform 0.2s ease-in-out;
-  }
-  
-  .container form .row .col .inputBox img:hover {
-    transform: scale(1.1);
-  }
-  
-  .container form .row .col .inputBox img:active {
-    transform: scale(0.9);
-  }
-  
-  .container form .row .col .flex {
-    display: flex;
-    gap: 10px;
-    margin-top: 30px;
-  }
-  
-  .container form .submit-btn {
-    width: 100%;
-    padding: 12px;
-    font-size: 17px;
-    margin-top: 10px;
-    background-color: #d78477;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-    transition: background-color 0.2s ease-in-out;
-  }
-  
-  .container form .submit-btn:hover {
-    background-color: #c47065;  
-  }
-  
-  .container form .submit-btn:active {
-    background-color: #e49d8f;
-  }
-  </style>
+
+  return sum % 10 === 0;
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap');
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 25px;
+  background-color: #f2f2f2;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.container form {
+  max-width: 500px;
+  background: #fff;
+  border: 2px solid #d78477;
+  border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+}
+
+.container form .row {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+}
+
+.container form .row .col {
+  flex: 1 1 50%;
+}
+
+.container form .row .col .inputBox {
+  margin-bottom: 15px;
+}
+
+.container form .row .col .inputBox label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: 700;
+}
+
+.container form .row .col .inputBox input {
+  width: 100%;
+  border: 1px solid #ccc;
+  padding: 10px 15px;
+  font-size: 15px;
+}
+
+.container form .row .col .inputBox input:focus {
+  border: 1px solid #d78477;
+}
+
+.container form .row .col .inputBox img {
+  height: 30px;
+  margin-top: 5px;
+  filter: drop-shadow(0 0 1px #000);
+  transition: transform 0.2s ease-in-out;
+}
+
+.container form .row .col .inputBox img:hover {
+  transform: scale(1.1);
+}
+
+.container form .row .col .inputBox img:active {
+  transform: scale(0.9);
+}
+
+.container form .row .col .flex {
+  display: flex;
+  gap: 10px;
+  margin-top: 30px;
+}
+
+.container form .submit-btn {
+  width: 100%;
+  padding: 12px;
+  font-size: 17px;
+  margin-top: 10px;
+  background-color: #d78477;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.2s ease-in-out;
+}
+
+.container form .submit-btn:hover {
+  background-color: #c47065;  
+}
+
+.container form .submit-btn:active {
+  background-color: #e49d8f;
+}
+</style>
